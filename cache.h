68c6ee1927c3f7f821aa0cbce3da097866333678
LegendaryHog <kuteinikov.sd@phystech.edu>
2022-09-17 19:07:54 +0300

    wrote private method replace()

--- cache.h
+++ cache.h
@@ -1,10 +1,11 @@
+#pragma once
 #include <iterator>
 #include <list>
 #include <unordered_map>
 #include <iostream>
-#pragma once
 
 namespace cache {
+
 template<typename T, typename KeyT = int>
 class cache_t 
 {
@@ -31,6 +32,63 @@ class cache_t
 
         using SubListIt = typename std::list<T_n_key>::iterator;        //I will keep only iterators on list in hash_map
         std::unordered_map<KeyT, SubListIt> hash_map;                   //for fast find elements in cache
+
+        void pop_back()
+        {
+            //delete from hash_map last elem from last sublist
+            hash_map.erase(cache.back().sublist.back().key);
+            //delete from last sublist last elem
+            cache.back().sublist.pop_back();
+
+            //if after this last sublist became empty - delete last node of cache
+            if (cache.back().sublist.size() == 0)               
+                cache.pop_back();
+            size--;
+        }
+
+        void push_back(T page, KeyT key)
+        {
+            //put elem in last sublist in back
+            cache.back().sublist.push_back({page, key, std::prev(cache.end())});
+            //put itr on elem in hash_map
+            hash_map[key] = std::prev(cache.back().sublist.end());
+            size++;
+        }
+
+        void replace (SubListIt itr)
+        {
+            //to simplify records
+            auto& cntr   = itr->itr_up->counter;
+            auto& itr_up = itr->itr_up;
+
+            //if found elem with biggest counter
+            //(in begin of cache)- create node with
+            //counter ub by 1
+            if (itr_up == cache.begin())
+                cache.push_front({{}, cntr + 1});
+
+            //if found elem not from begin - check existance of node
+            //with counter ub by 1 - create if its dont exist
+            else if (std::prev(itr_up)->counter != cntr + 1)
+                cache.insert(itr_up, {{}, cntr + 1});
+
+            //for simplify records
+            auto prev_itr_up = std::prev(itr_up);
+
+            //replace in back of sublist with counter up by 1 found node (its reconnect the pointers,no copy)
+            prev_itr_up->sublist.splice(prev_itr_up->sublist.end(), itr_up->sublist, itr);
+
+            //if after replace sublist in node
+            //in cache with counter of hit became
+            //empty - delete
+            if (itr_up->sublist.size() == 0)
+                cache.erase(itr_up);
+
+            //connect iterator on node of
+            //cache in found elem on new node
+            itr_up = prev_itr_up;
+        }
+
     public:
         cache_t(std::size_t cap): capacity {cap}, size {0} {}; //constructor by fix size
         cache_t() {};
@@ -46,54 +104,28 @@ class cache_t
             if (hit == hash_map.end()) //if not found key
             {
                 if (full()) //if full
-                {
-                    hash_map.erase(cache.back().sublist.back().key);    //delete from hash_map last elem from last sublist
-                    cache.back().sublist.pop_back();                    //delete from last sublist last elem
-                    //if after this last sublist became empty - delete last node of cache
-                    if (cache.back().sublist.size() == 0)               
-                        cache.pop_back();
-                    size--;
-                }
-                //make node with counter 1 in cache if cache is empty
+                    //pop element from
+                    //back from back sublist
+                    pop_back();
+
+                //make node with counter 1 in
+                //cache if cache is empty
                 if (empty())
                     cache.push_back({{}, 1});
-                //make node with counter 1 in cache if its not exist
+
+                //make node with counter 1 in
+                //cache if its not exist
                 if (cache.back().counter != 1)
                     cache.push_back({{}, 1});
-                //put elem in last sublist in back
-                cache.back().sublist.push_back({slow_get_page(key), key, std::prev(cache.end())});
-                //put itr on elem in hash_map
-                hash_map[key] = std::prev(cache.back().sublist.end());
-                size++;
+
+                //push elem in sublist with counter 1 in back
+                push_back(slow_get_page(key), key);
             
                 return false;
             }
-            else //if found
-            {
-                //to simplify records
-                auto  cntr_hit  = hit->second->itr_up->counter;
-                auto& itr_hit   = hit->second;
-                auto& itr_up_hit= hit->second->itr_up;
-
-                //if found elem with biggest counter (in begin of cache)- create node with counter ub by 1
-                if (itr_up_hit == cache.begin())
-                    cache.push_front({{}, cntr_hit + 1});
-                //if found elem not from begin - check existance of node with counter ub by 1 - create if its dont exist
-                else if (std::prev(itr_up_hit)->counter != cntr_hit + 1)
-                    cache.insert(itr_up_hit, {{}, cntr_hit + 1});
-
-                auto prev_itr_up_hit = std::prev(itr_up_hit); //for simplify records
-                //replace in back of sublist with counter up by 1 found node (its reconnect the pointers,no copy)
-                prev_itr_up_hit->sublist.splice(prev_itr_up_hit->sublist.end(), itr_up_hit->sublist, itr_hit);
-
-                //if after replace sublist in node in cache with counter of hit became empty - delete
-                if (itr_up_hit->sublist.size() == 0)
-                    cache.erase(itr_up_hit);
-
-                //connect iterator on node of cache in dound elem on new node
-                itr_up_hit = prev_itr_up_hit;
-                return true;
-            }
+            //if found
+            replace (hit->second);
+            return true;
         }
 
         void dump() const
